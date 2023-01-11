8c492aac7ef28af4f60fea4700459d4d90eeb5a4
Manuel <122300441+manuelule@users.noreply.github.com>
2023-01-11 18:53:45 +0100

    Add files via upload

---
+++ hanoi-nr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void hanoi(int n, char origen, char destino, char auxiliar) {
+    int i;
+    for (i = n; i >= 1; i--) {
+        hanoi(i-1, origen, auxiliar, destino);
+        printf("Mover disco %d de %c a %c\n", i, origen, destino);
+        hanoi(i-1, auxiliar, destino, origen);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int n = 3; // number of disks
+    if (argc > 1) {
+        n = atoi(argv[1]);
+    }
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
+}
---
+++ hanoi-nr.py
@@ -0,0 +1,12 @@
+def Hanoi (discos,TorreOrigen=1,TorreAuxiliar=2,TorreDestino=3):
+    if discos==1:
+        print (TorreOrigen,"a",TorreDestino)
+        
+    else:
+        Hanoi(discos-1,TorreOrigen,TorreDestino,TorreAuxiliar)
+        print(TorreOrigen,"a",TorreDestino)
+        Hanoi (discos-1,TorreAuxiliar,TorreOrigen,TorreDestino)
+    return
+
+##Lo probamos con 3 discos
+Hanoi(3)
